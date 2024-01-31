e7b3749a4bba77e74bd79b59e08e7a9e5292188e
Joshua Garcia <garcia.jc@utexas.edu>
2024-01-31 12:53:56 -0600

    Assignment

--- Assignment2/intervals.py
+++ Assignment2/intervals.py
@@ -5,11 +5,9 @@
 """
 import sys
 
-tup_list = ["14 17", "15 -5", "26 29", "-20 -15", "12 15", "2 3",
+tup_list = ["14 17", "-8 -5", "26 29", "-20 -15", "12 15", "2 3",
             "-10 -7", "25 30", "2 4", "-21 -16", "13 18", "22 27",
             "-6 -3", "3 6", "-25 -14"]
-
-
 
 tuples_list = []
 for m_tuple in tup_list:
@@ -17,24 +15,12 @@ for m_tuple in tup_list:
     tuples_list.append(tuple((int(tup[0]), int(tup[1]))))
 
 def merge_tuples (tuples_list):
-    """Merge the tuples"""
+    """Merge the tuples"""        
     sort_list = [list(i) for i in sorted(tuples_list)]
     result = [sort_list.pop(0)]
     
     sort_iterator = 0
     result_iterator = 0
-
-    # print(result[result_iterator][1])
-    # print(sort_list[sort_iterator][1] - 1)
-
-    # print(result[result_iterator][1] == (sort_list[sort_iterator][0] - 1))
-
-    # print(sort_list[sort_iterator][0])
-    # print(sort_list[sort_iterator][1])
-
-    # print(result[result_iterator][0])
-    # print(result[result_iterator][1])
-    
 
     while len(sort_list) >= 1:
         # Overlap, where second element ends after the first
@@ -58,40 +44,18 @@ def merge_tuples (tuples_list):
             del sort_list[sort_iterator]
         # No overlap, but second point of first element and first point of second 
         # element touch
-        elif (result[result_iterator][1]) == (sort_list[sort_iterator][0] - 1):
-            result[result_iterator][1] = sort_list[sort_iterator][1]
-            del sort_list[sort_iterator]
-
-
-
+        # elif (result[result_iterator][1]) == (sort_list[sort_iterator][0] - 1):
+        #     result[result_iterator][1] = sort_list[sort_iterator][1]
+        #     del sort_list[sort_iterator]
+        # No overlap at all
         else:
             result.append(sort_list[sort_iterator])
             result_iterator += 1
             del sort_list[sort_iterator]
 
-    return result
+    result = [tuple(i) for i in result]
 
-
-print(merge_tuples(tuples_list))
-    
-
-
-
-    # for i in range(len(tuples_list) - 1):
-    #     temp_tuple_1 = tuples_list[i][0]
-    #     temp_tuple_2 = tuples_list[i][1]
-    #     for j in range(i + 1, len(tuples_list)):
-    #         if (min(tuples_list[i]) > min(tuples_list[j]) and
-    #             max(tuples_list[i]) > max(tuples_list[j])):
-    #             # tuple_merged.append((min(tuples_list[j]), tuples_list[i][1]))
-    #             temp_tuple_1 = min(tuples_list[j])
-    #             # tuples_list[i][0] = min(tuples_list[j])
-            
-    #         elif (max(tuples_list[i]) < max(tuples_list[j]) and
-    #             min(tuples_list[i]) < min(tuples_list[j])):
-    #             temp_tuple_2 = max(tuples_list[j])
-    #             # tuples_list[i][1] = max(tuples_list[i])
-                                                  
+    return result                                 
 
 def sort_by_interval_size (tuples_list):
     """
@@ -100,40 +64,48 @@ def sort_by_interval_size (tuples_list):
     size of the interval if two intervals have the size then it will sort by the
     lower number in the interval
     """
+    result = merge_tuples(tuples_list)
+    sort = result.copy()
 
-    # Add Your Code HERE ... AND REMOVE THIS Line
+    for i in range(len(sort) - 1):
+        min_int = i
+        for j in range(i + 1, len(sort)):
+            if (max(sort[j]) - min(sort[j]) 
+                < max(sort[min_int]) - min(sort[min_int])):
+                min_int = j
 
-    return None # Replace this.
+        if min_int != i:
+            temp = sort[i]
+            sort[i], sort[min_int] = sort[min_int], temp
 
-
+    return sort
 
 def main():
     """
     Open file intervals.in and read the data and create a list of tuples
     """
-    # sys.stdin.readline()
+    sys.stdin.readline()
 
     tup_list = ["14 17", "-8 -5", "26 29", "-20 -15", "12 15", "2 3",
                 "-10 -7", "25 30", "2 4", "-21 -16", "13 18", "22 27",
                 "-6 -3", "3 6", "-25 -14"]
-    # tup_list = sys.stdin.readlines()
+    tup_list = sys.stdin.readlines()
 
-    # Editing this for now just to manually input tuple pairs
-    # tuples_list = []
-    # for m_tuple in tup_list:
-    #     tup = m_tuple.split()
-    #     tuples_list.append(tuple((int(tup[0]), int(tup[1]))))
+    tuples_list = []
+    for m_tuple in tup_list:
+        tup = m_tuple.split()
+        tuples_list.append(tuple((int(tup[0]), int(tup[1]))))
 
-    # print(tuples_list)
+    print(tuples_list)
     # merge the list of tuples
-    # merged = merge_tuples(tuples_list)
+    merged = merge_tuples(tuples_list)
 
     # sort the list of tuples according to the size of the interval
-    # sorted_merge = sort_by_interval_size(merge_tuples(tuples_list))
+    sorted_merge = sort_by_interval_size(merge_tuples(tuples_list))
 
     # write the output list of tuples from the two functions
-    # print(merged)
-    # print(sorted_merge)
+    print(merged)
+    print(sorted_merge)
 
 if __name__ == "__main__":
     main()
